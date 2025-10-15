10fd2193bb40ba7b17b7bcedbfd33ac1545707b6
CC3031 <acunafernando617@gmail.com>
2025-10-15 13:44:00 -0500

    rollback att

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
-import AuthenticatorWrapper from "./AuthenticatorWrapper";
 import "@aws-amplify/ui-react/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>      
-        <AuthenticatorWrapper>
-          {children}
-        </AuthenticatorWrapper>
+        {children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-import { useAuthenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
 
@@ -15,7 +14,6 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const { signOut } = useAuthenticator();
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -53,7 +51,6 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
-      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
