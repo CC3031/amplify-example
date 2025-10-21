96431104066f6590ccbefdcae7c1f85d96eb417e
CC3031 <acunafernando617@gmail.com>
2025-10-20 20:46:25 -0500

    changing main page

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,26 @@ export default function App() {
 
   return (
     <main>
-      <h1>Berserk Appreciation Website</h1>
+      <header>
+        <h1>Berserk Arc Analysis Website</h1>
+        <nav>
+          <a className="links" href="">Black Swordsman</a>
+          <a className="links" href="">Golden Age</a>
+          <a className="links" href="">Conviction</a>
+          <a className="links" href="">Falcon of the Millennium</a>
+          <a className="links" href="">Fantasia</a>
+
+        </nav>
+      </header>
+      <div id="imgDiv">
+        <img src="https://tse1.mm.bing.net/th/id/OIP.3JAvD_D10Kco9whN7NjJ3wHaId?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3"/>
+      </div>
+
+      <footer>
+        <a id="signIn" href="">Want access to individual character analysis? Sign in for free!</a>
+        {/* Authentication here */}
+      </footer>
+      
     </main>
   );
 }
